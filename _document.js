958f641816881cc3116824fe283545bac439acee
amunozf2021 <91094909+amunozf2021@users.noreply.github.com>
2025-12-03 00:37:49 +0100

    Delete _document.js

--- _document.js
+++
@@ -1,4 +0,0 @@
-import { Html, Head, Main, NextScript } from "next/document";
-export default function Document() {
-  return (<Html lang="es"><Head /><body className="dark"><Main /><NextScript /></body></Html>);
-}
